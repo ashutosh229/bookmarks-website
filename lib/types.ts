b2edb01449799658a7a9587edbf10f846ae6d0b4
ashutosh229 <akumarjha875@gmail.com>
2025-03-29 06:04:41 +0530

    added the types for income, expenses and due record

--- lib/types.ts
+++ lib/types.ts
@@ -15,3 +15,28 @@ export interface ExamRecord {
     weightage: number;
     created_at?: Date;
 }
+
+export type ExpenseRecord = {
+    id?: string;
+    amount: number;
+    description: string;
+    includeInTotal: boolean;
+    created_at?: string;
+};
+
+export type IncomeRecord = {
+    id?: string;
+    amount: number;
+    description: string;
+    created_at?: string;
+};
+
+export type DueRecord = {
+    id?: string;
+    amount: number;
+    description: string;
+    person: string;
+    isPaid: boolean;
+    isOptional: boolean;
+    created_at?: string;
+};

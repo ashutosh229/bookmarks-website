0807831aca70b6731536384338a1ca8d850a6728
ashutosh229 <akumarjha875@gmail.com>
2025-09-03 15:00:38 +0530

    pushed the socials data

---
+++ data/socials.ts
@@ -0,0 +1,18 @@
+import { Github, Linkedin, BriefcaseBusiness } from "lucide-react";
+
+const socials = [
+  {
+    link: "https://github.com/ashutosh229",
+    icon: Github,
+  },
+  {
+    link: "https://linkedin.com/in/ashutosh-kumar-jha-601098280",
+    icon: Linkedin,
+  },
+  {
+    link: "https://portfolio-ten-plum-37.vercel.app/",
+    icon: BriefcaseBusiness,
+  },
+];
+
+export default socials;

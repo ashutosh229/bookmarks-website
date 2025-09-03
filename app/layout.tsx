57ff8ed4840703b19b9691d0ae42f4c89a16842e
ashutosh229 <akumarjha875@gmail.com>
2025-09-03 15:40:04 +0530

    refactored the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import type { Metadata } from "next";
-import { ThemeProvider } from "next-themes";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Providers from "./providers";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,22 +15,22 @@ export const metadata: Metadata = {
   icons: "./logo.png",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-grow">{children}</main>
-            <Footer />
-          </div>
-        </ThemeProvider>
+        <Providers
+          children={
+            <div className="flex flex-col min-h-screen">
+              <Header />
+              <main className="flex-grow">{children}</main>
+              <Footer />
+            </div>
+          }
+        ></Providers>
       </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
